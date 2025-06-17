ee08712c36998c79cb08036bff53040be3d29398
erickWillyan-Baymetrics <erick.cruz@baymetrics.com.br>
2025-06-17 12:41:09 -0300

    Feat: Initial single-spa configuration

---
+++ src/root.component.tsx
@@ -0,0 +1,7 @@
+export default function Root(props) {
+  return (
+    <div className="bg-blueMedium">
+      <p>TEste</p>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        aristotelica: ['"Aristotelica Demibold"', "sans-serif"],
+        aristotelicaRegular: ['"Aristotelica Demibold Regular"', "sans-serif"],
+        aristotelicaSmallCaps: [
+          '"Aristotelica SmallCaps-Regular"',
+          "sans-serif",
+        ],
+      },
+      colors: {
+        blueLight: "#EAE7F7",
+        blueMedium: "#2C5B8C",
+        grayLight: "#F0F4F8",
+        grayPrimary: "#D1D1D1",
+        grayMedium: "#555555",
+        grayDark: "#333333",
+      },
+      width: {
+        "80%": "80%",
+        "90%": "90%",
+      },
+    },
+  },
+  plugins: [],
+};
